e77db3a3769ec41e846e4eb50808e344f85bf9da
tyriem <tyrie.moss@gmail.com>
2021-05-18 21:23:24 -0400

    CISCO UPDATE

---
+++ CISCO/CISCO 27 - Comparison Operators.py
@@ -0,0 +1,12 @@
+### AUTHOR: TMRM   
+### PROJECT: CISCO DevNet - Comparison
+### VER: 1.0
+### DATE: 05-XX-2021
+
+### OBJECTIVE ###
+# Using one of the comparison operators in Python, write a simple two-line program that takes the parameter n as input, which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
+#Don't create any if blocks (we're going to talk about them very soon). Test your code using the data we've provided for you.
+### OBJECTIVE ###
+
+n = int(input("GOOD DAY PLEASE ENTER A NUMBER: "))
+print(100 <= n)
