3703d17e809b5f5a949090bb7bf3c7b21470afcd
tyriem <tyrie.moss@gmail.com>
2021-06-25 11:07:07 -0400

    UPDATE CISCO

--- CISCO/CISCO 49 - MEDIA ACCESS - ALIEN/Welcome Earthling.py
+++ CISCO/CISCO 49 - MEDIA ACCESS - ALIEN/Welcome Earthling.py
@@ -17,31 +17,44 @@ print('''
 
 ### Declare CALLs & DEFs ###
 
+# IMPORT MODULE tkinter
 from tkinter import*
 
+# SET root to Tk()
 root = Tk()
+
+# SET TITLE for Window
 root.title("Welcome To Earth!")
+
+# SET Windows geo Size
 root.geometry('800x600-5+40')
 
+
+# DEF func showAlien
 def showAlien():           
     labelImg.pack()
 
+# DEF func hideAlien
 def hideAlien():
     labelImg.pack_forget()
 
+# DEF func buttonQuit
 def buttonQuit():
     root.destroy()
 
 
 ### BUTTON LOGIC ###
 
+#BUTTON Show
 buttonON = Button(root, text="Show", width=5, height=2, command=showAlien)
 buttonON.pack(side=LEFT) #tell tkinter to put objects in one row
 
+#BUTTON Off
 buttonOFF = Button(root, text="Hide", width=5, height=2, command=hideAlien)
 buttonOFF.pack(side=LEFT) #tell tkinter to put objects in one row
 
-buttonQUIT = Button(root, text="E.T. GO HOME!", width=10, height=2, command=buttonQuit)
+#BUTTON EXIT
+buttonQUIT = Button(root, text="E.T. GO HOME", width=10, height=2, command=buttonQuit)
 buttonQUIT.pack(side=LEFT) #tell tkinter to put objects in one row
 
 ### IMAGE LOGIC ###
